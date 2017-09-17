a1e394f040ca126ce79a8c7ab43210983b7f6559
betwar512 <betwar512@gmail.com>
2017-09-17 17:37:32 +1000

    new classes

---
+++ src/main/java/net/endpoint/filesystem/model/FsGridFsFile.java
@@ -0,0 +1,57 @@
+package net.endpoint.filesystem.model;
+
+import java.util.Date;
+
+import javax.persistence.Id;
+
+public class FsGridFsFile {
+
+	@Id
+	private long id;
+	private String name;
+	private String description;
+	private Date timestamp;
+	private Date lastUpdate;
+	
+	
+	
+	
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Date getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(Date timestamp) {
+		this.timestamp = timestamp;
+	}
+	public Date getLastUpdate() {
+		return lastUpdate;
+	}
+	public void setLastUpdate(Date lastUpdate) {
+		this.lastUpdate = lastUpdate;
+	}
+	
+	
+	
+	
+	
+	
+	
+}
